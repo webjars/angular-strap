5bb436a91f9226c4f5d69da6127e0b848f0554b5
James Ward <james@jamesward.org>
2014-03-08 10:33:49 -0700

    fixes #15

--- src/main/resources/webjars-requirejs.js
+++ src/main/resources/webjars-requirejs.js
@@ -1,4 +1,4 @@
 requirejs.config({
-    paths: { "angular-strap": webjars.path("angular-strap", "angular") },
+    paths: { "angular-strap": webjars.path("angular-strap", "angular-strap") },
     shim: { "angular-strap": [ "angular" ] }
 });
